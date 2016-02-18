b03d464eba3228b6ec7c6a1ec5a090d2a2fdcb6d
kevin deutscher <kevin.m.deutscher@gmail.com>
2016-02-18 00:17:42 -0500

    pulls all anchors and emails from mit

--- crawler.py
+++ crawler.py
@@ -1,33 +1,37 @@
 import urllib.request as ur
 import re
-#BeautifulSoup --needs to be installed on user computer
 from bs4 import BeautifulSoup
 urls = set()
+allemails = set()
 
 
 pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
 sitefile = ur.urlopen("http://web.mit.edu/comment-form.html")
-site_base = ur.urlopen("http://web.mit.edu")
+site_base = ur.urlopen("http://mit.edu")
 sitetext = sitefile.read()
 emails = re.findall(pattern,sitetext)
 soup = BeautifulSoup(sitetext, "lxml")
 
 
 for anchor in soup.find_all("a"):
-
-    # extract link url from the anchor
     link = anchor.attrs["href"] if "href" in anchor.attrs else ''
-    # print(link)
-    # resolve relative links
     if link.startswith('http'):
         urls.add(link)
-        # print(link)
 
+for link in urls:
+    pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
+    sitefile = ur.urlopen(link)
+    sitetext = sitefile.read()
+    emails = re.findall(pattern,sitetext)
+    # print(emails)
 
+    for subemail in emails:
+        allemails.add(subemail)
+        # print(subemail)
+    # allemails.add(emails)
+    # print(link)
 
-
-
-# print (emails)
+print (allemails)
 # print (sitetext)
-print (urls)
+# print (urls)
 # print (soup)
