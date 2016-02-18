940b020c0cd653938bec573f0e4040cd02c5a611
kevin deutscher <kevin.m.deutscher@gmail.com>
2016-02-17 23:35:28 -0500

    works on mit page for all emails, jana still doesn't render full page

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@
 import urllib.request as ur
 # regex
 import re
+#BeautifulSoup
+from bs4 import BeautifulSoup
 
 # string
 # stringforregex = '<a\s+href=\"mailto:([a-zA-z0-9._@]*)\">"'
@@ -10,13 +12,16 @@ import re
 
 pattern = re.compile(b"<a\s+href=\"mailto:([a-zA-Z0-9._@]*)\">", re.IGNORECASE)
 
-sitefile = ur.urlopen("http://jana.com/contact")
+sitefile = ur.urlopen("http://web.mit.edu/comment-form.html")
 sitetext = sitefile.read()
-emails = re.findall(pattern,sitetext)
-# emails = re.findall(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",sitetext)
+# emails = re.findall(pattern,sitetext)
+emails = re.findall(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",sitetext)
 
-title = re.findall(b"<title>(.+?)</title>",sitetext)
+# title = re.findall(b"<title>(.+?)</title>",sitetext)
 
-# print (emails)
+soup = BeautifulSoup(sitetext)
+
+print (emails)
 # print (sitetext)
-print (title)
+# print (title)
+# print (soup)
