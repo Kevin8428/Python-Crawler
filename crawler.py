7eca4e8fffe3f1c71cc0c4bbd8c1840c6a364f8c
kevin deutscher <kevin.m.deutscher@gmail.com>
2016-02-18 00:35:19 -0500

    refactoring

--- crawler.py
+++ crawler.py
@@ -1,37 +1,30 @@
 import urllib.request as ur
 import re
 from bs4 import BeautifulSoup
+from pprint import pprint
 urls = set()
 allemails = set()
 
-
-pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
+# set page html to variable
 sitefile = ur.urlopen("http://web.mit.edu/comment-form.html")
-site_base = ur.urlopen("http://mit.edu")
 sitetext = sitefile.read()
-emails = re.findall(pattern,sitetext)
 soup = BeautifulSoup(sitetext, "lxml")
 
-
+# ID all urls and push to global variable
 for anchor in soup.find_all("a"):
     link = anchor.attrs["href"] if "href" in anchor.attrs else ''
     if link.startswith('http'):
         urls.add(link)
 
+# cycle thru urls, ID emails and push to global variable
 for link in urls:
     pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
     sitefile = ur.urlopen(link)
     sitetext = sitefile.read()
     emails = re.findall(pattern,sitetext)
-    # print(emails)
 
     for subemail in emails:
         allemails.add(subemail)
-        # print(subemail)
-    # allemails.add(emails)
-    # print(link)
 
-print (allemails)
-# print (sitetext)
-# print (urls)
-# print (soup)
+pprint (allemails)
+# pprint (urls)
