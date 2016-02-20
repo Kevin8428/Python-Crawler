10cccaf2bfa1286422120cd0b93166aa74be0e2b
kevin deutscher <kevin.m.deutscher@gmail.com>
2016-02-20 15:06:15 -0500

    accepts command line arguments without http or html included

--- crawler.py
+++ crawler.py
@@ -1,22 +1,22 @@
 import urllib.request as ur
+from urllib.request import Request, urlopen
 import re
 from bs4 import BeautifulSoup
 from pprint import pprint
 import sys
+# import mechanize
+sys.path
+# currently works for web.mit.edu/comment-form.html
 
 urls = set()
 allemails = set()
-# sitefile = set()
-sitefile = ur.urlopen(sys.argv[1])
 
-# if not (sys.argv[1]).startswith("http://"):
-#     sitefile = ("https://" + ur.urlopen(sys.argv[1]))
-# else:
-#     sitefile = ur.urlopen(sys.argv[1])
-# set page html to variable
+req = Request('http://' + sys.argv[1], headers={'User-Agent': 'Mozilla/5.0'})
+sitetext = urlopen(req).read()
 
+# sitefile = ur.urlopen('http://' + sys.argv[1])#WORKS
+# sitetext = sitefile.read()
 
-sitetext = sitefile.read()
 soup = BeautifulSoup(sitetext, "lxml")
 
 # ID all urls and push to global variable
@@ -28,9 +28,14 @@ for anchor in soup.find_all("a"):
 # cycle thru urls, ID emails and push to global variable
 for link in urls:
     pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
-    sitefile = ur.urlopen(link)
-    sitetext = sitefile.read()
+    # sitefile = ur.urlopen(link)
+    # sitetext = sitefile.read()
+    # emails = re.findall(pattern,sitetext)
+
+    req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+    sitetext = urlopen(req).read()
     emails = re.findall(pattern,sitetext)
+
 
     for subemail in emails:
         allemails.add(subemail)
