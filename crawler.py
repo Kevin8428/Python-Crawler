926eaf1a677f3c07d20f6195c07fcd30aaa13d2c
kevin deutscher <kevin.m.deutscher@gmail.com>
2016-02-20 15:34:35 -0500

    works but need to exclude subdomains, still breaks on jana blog for unknown reason

--- crawler.py
+++ crawler.py
@@ -4,7 +4,6 @@ import re
 from bs4 import BeautifulSoup
 from pprint import pprint
 import sys
-# import mechanize
 sys.path
 # currently works for web.mit.edu/comment-form.html
 
@@ -20,26 +19,22 @@ sitetext = urlopen(req).read()
 soup = BeautifulSoup(sitetext, "lxml")
 
 # ID all urls and push to global variable
-for anchor in soup.find_all("a"):
-    link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+for each in soup.find_all("a"):
+    link = each.attrs["href"] if "href" in each.attrs else ''
+    print (link)
     if link.startswith('http'):
         urls.add(link)
 
 # cycle thru urls, ID emails and push to global variable
 for link in urls:
     pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
-    # sitefile = ur.urlopen(link)
-    # sitetext = sitefile.read()
-    # emails = re.findall(pattern,sitetext)
 
     req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
     sitetext = urlopen(req).read()
     emails = re.findall(pattern,sitetext)
 
-
     for subemail in emails:
         allemails.add(subemail)
 
-pprint (allemails)
 
-# pprint (urls)
+# pprint (allemails)
