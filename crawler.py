e3d043cfff76dcb7d2afc650eb68807cc164157f
kevin deutscher <kevin.m.deutscher@gmail.com>
2016-02-20 15:55:53 -0500

    need to exclude subdomains otherwise functional look into setting root wtih urlparse

--- crawler.py
+++ crawler.py
@@ -21,20 +21,21 @@ soup = BeautifulSoup(sitetext, "lxml")
 # ID all urls and push to global variable
 for each in soup.find_all("a"):
     link = each.attrs["href"] if "href" in each.attrs else ''
-    print (link)
-    if link.startswith('http'):
-        urls.add(link)
+    urls.add(link)
 
 # cycle thru urls, ID emails and push to global variable
 for link in urls:
+    # print(link)
     pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
 
-    req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(link, headers={'User-Agent': 'Mozilla/44.0.2'})
     sitetext = urlopen(req).read()
     emails = re.findall(pattern,sitetext)
+    # print(emails)
 
     for subemail in emails:
         allemails.add(subemail)
 
 
-# pprint (allemails)
+pprint (allemails)
+# pprint (urls)
