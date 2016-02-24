8124d268d00afc99a0b09f883dda7bdb6609ee12
kevin deutscher <kevin.m.deutscher@gmail.com>
2016-02-24 18:29:07 -0500

    update

--- crawler.py
+++ crawler.py
@@ -8,34 +8,40 @@ sys.path
 # currently works for web.mit.edu/comment-form.html
 
 urls = set()
+urlsupdate = set()
 allemails = set()
 
-req = Request('http://' + sys.argv[1], headers={'User-Agent': 'Mozilla/5.0'})
+req = Request('http://' + sys.argv[1], headers={'User-Agent': 'Mozilla/44.0.2'})
 sitetext = urlopen(req).read()
 
-# sitefile = ur.urlopen('http://' + sys.argv[1])#WORKS
-# sitetext = sitefile.read()
 
 soup = BeautifulSoup(sitetext, "lxml")
 
 # ID all urls and push to global variable
 for each in soup.find_all("a"):
     link = each.attrs["href"] if "href" in each.attrs else ''
+    if 'http' not in link:
+        link = ('http://' + sys.argv[1]+ link)
     urls.add(link)
+    # print(link)
+
+
+for each in urls:
+    start = ('http://' + sys.argv[1])
+    if start in each:
+        urlsupdate.add(each)
 
 # cycle thru urls, ID emails and push to global variable
-for link in urls:
-    # print(link)
-    pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
-
-    req = Request(link, headers={'User-Agent': 'Mozilla/44.0.2'})
-    sitetext = urlopen(req).read()
-    emails = re.findall(pattern,sitetext)
-    # print(emails)
-
-    for subemail in emails:
-        allemails.add(subemail)
-
+for link in urlsupdate:
+    try:
+        pattern = re.compile(b"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.IGNORECASE)
+        req = Request(link, headers={'User-Agent': 'Mozilla/44.0.2'})
+        sitetext = urlopen(req).read()
+        emails = re.findall(pattern,sitetext)
+        for subemail in emails:
+            allemails.add(subemail)
+    except:
+        pass
 
 pprint (allemails)
-# pprint (urls)
+# pprint (urlsupdate)
